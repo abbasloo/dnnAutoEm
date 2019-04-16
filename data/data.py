00089636badf041e8c8e3ac1d7c128d5451506b2
Amin Abbasloo <abbasloo@cs.uni-bonn.de>
2019-04-16 14:49:22 +0200

    Add files via upload

--- data/data.py
+++ data/data.py
@@ -3,12 +3,7 @@ import os
 
 
 
-comp = ['S2000001', 'S2000002', 'S2000003',
-	'S2000004', 'S2000040', 'S2000041',
-	'S2000046', 'S2000047', 'S2000048', 
-	'S2000049', 'S2000054', 'S2000055', 
-	'S2000056', 'S2000062', 'S2000071', 
-	'S2000072', 'S2000149']
+comp = ['S2000001']
 
 target = 1515*1.0
 ratio = [target/1714, target/1736, target/1519, target/1705]
@@ -27,14 +22,14 @@ for c in range(1, 299+1):
 		log = 'open d3plot "/home/ndv/stud/data/Truck/' + nameHere + '/d3plot"' + '\n'
 		log += 'selectpart on ' + comp[cp] + '/0' + '\n'
 		for i in range(1, 60+1):
-			log += 'output "/home/mabbasloo/Documents/carData4/f' + '{:03d}'.format(c) + '_' + comp[cp] + '_' + np.str(i) + '.stl" ' + np.str(i) + ' 7 0 0' + '\n'
+			log += 'output "/home/mabbasloo/Documents/carCrashData/S2000001/f' + '{:03d}'.format(c) + '_' + comp[cp] + '_' + np.str(i) + '.stl" ' + np.str(i) + ' 7 0 0' + '\n'
 		log += 'stop'
-		file = open('/home/mabbasloo/Documents/carData4/Data.cfile','w') 
+		file = open('/home/mabbasloo/Documents/carCrashData/S2000001/Data.cfile','w') 
 		file.write(log) 
 		file.close()
 		os.system('/home/mabbasloo/Documents/lsprepost4.0_centos6/lspp4 Data.cfile')
 		for i in range(1, 60+1):
-			fname = '/home/mabbasloo/Documents/carData4/f' + '{:03d}'.format(c) + '_' + comp[cp] + '_' + np.str(i)
+			fname = '/home/mabbasloo/Documents/carCrashData/S2000001/f' + '{:03d}'.format(c) + '_' + comp[cp] + '_' + np.str(i)
 			os.system('/home/mabbasloo/meshconv ' + fname + '.stl ' + '-c obj -o ' + fname)
 			#os.system('/home/mabbasloo/simplify ' + fname + '.obj ' + fname + '.obj ' + np.str(ratio[cp]))
 			os.system('/home/mabbasloo/meshconv ' + fname + '.obj ' + '-c off -o ' + fname)
---
+++ data/run.sh
@@ -0,0 +1,13 @@
+cd /home/mabbasloo/Documents/carCrashData/S2000001/
+python2 data.py
+
+cd /home/mabbasloo/Documents/carCrashData/S2000002/
+python2 data.py
+
+cd /home/mabbasloo/Documents/carCrashData/S2000003/
+python2 data.py
+
+cd /home/mabbasloo/Documents/carCrashData/S2000004/
+python2 data.py
+
+
